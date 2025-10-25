26464fd64d432995c9f506daa12e8f164d67e05d
Brian-Podolskiy <147195222+Brian-Podolskiy@users.noreply.github.com>
2025-10-25 15:42:39 -0400

    fixed "one second deduction" bug in timer.py

--- timer.py
+++ timer.py
@@ -7,8 +7,13 @@ def countdown(t):
     while (t):
         if (keyboard.is_pressed("p") == True): # if p is held down the timer pauses
             print("paused")
-            time.sleep(1) # issue: time still counts down for one second after p is held
+            time.sleep(1)
+            if (secondAdded == False): # adds one second to account for issue of one second being deducted on pause
+                t += 1
+                secondAdded = True
+            
         else:
+            secondAdded = False
             mins, secs = divmod(t, 60) # converts secs to minutes and seconds
             timer = "{:02d}:{:02d}".format(mins, secs)
             print(timer, end='\r')
@@ -17,3 +22,4 @@ def countdown(t):
       
 t = int(input("enter time in seconds: "))
 countdown(t)
+
