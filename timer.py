c022fd11b3aa4b458876fd3b9546577bf00d2b8a
Brian-Podolskiy <147195222+Brian-Podolskiy@users.noreply.github.com>
2025-10-25 16:01:06 -0400

    now asks if user wants to restart or quit

--- timer.py
+++ timer.py
@@ -1,25 +1,40 @@
 import time
 import keyboard
 
-pause = False
+running = True
 
-def countdown(t):
-    while (t):
-        if (keyboard.is_pressed("p") == True): # if p is held down the timer pauses
-            print("paused")
-            time.sleep(1)
-            if (secondAdded == False): # adds one second to account for issue of one second being deducted on pause
-                t += 1
-                secondAdded = True
-            
+while (running == True):
+    def countdown(t):
+        while (t):
+            if (keyboard.is_pressed("p") == True): # if p is held down the timer pauses
+                print("paused", end = '\r')
+                time.sleep(1)
+                if (secondAdded == False): # adds one second to account for issue of one second being deducted on pause
+                    t += 1
+                    secondAdded = True
+                
+            else:
+                print("             ", end = "\r") # clears line
+                secondAdded = False
+                mins, secs = divmod(t, 60) # converts secs to minutes and seconds
+                timer = "{:02d}:{:02d}".format(mins, secs)
+                print(timer, end = '\r')
+                time.sleep(1)
+                t -= 1
+        
+    t = int(input("enter time in seconds: "))
+    countdown(t)
+    print("Timer finished!!")
+    answered = False
+
+    while (answered == False):
+        contProg = input("do you want to run again? (y/n): ")
+        if (contProg == "y" or contProg == "Y"):
+            print("restarting...")
+            answered = True
+        elif (contProg == "n" or contProg == "N"):
+            print ("quitting...")
+            answered = True
+            running = False
         else:
-            secondAdded = False
-            mins, secs = divmod(t, 60) # converts secs to minutes and seconds
-            timer = "{:02d}:{:02d}".format(mins, secs)
-            print(timer, end='\r')
-            time.sleep(1)
-            t -= 1
-      
-t = int(input("enter time in seconds: "))
-countdown(t)
-
+            print("error, bad value, either y or n")
