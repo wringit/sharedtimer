6e4333a7a6b3ee2c9118ddd1bac78a58a582ec1b
Brian-Podolskiy <147195222+Brian-Podolskiy@users.noreply.github.com>
2025-10-25 15:39:03 -0400

    let users pause the timer
    timer can now be paused by holding down the 'p' key

--- timer.py
+++ timer.py
@@ -1,13 +1,19 @@
-import time;
+import time
+import keyboard
+
+pause = False
 
 def countdown(t):
     while (t):
-        mins, secs = divmod(t, 60) # converts secs to minutes and seconds
-        timer = "{:02d}:{:02d}".format(mins, secs)
-        print(timer, end='\r')
-        time.sleep(1)
-        t-=1
-        #print("result: " + str(t))
-
-t = int(input("enter time in s: "))
+        if (keyboard.is_pressed("p") == True): # if p is held down the timer pauses
+            print("paused")
+            time.sleep(1) # issue: time still counts down for one second after p is held
+        else:
+            mins, secs = divmod(t, 60) # converts secs to minutes and seconds
+            timer = "{:02d}:{:02d}".format(mins, secs)
+            print(timer, end='\r')
+            time.sleep(1)
+            t -= 1
+      
+t = int(input("enter time in seconds: "))
 countdown(t)
